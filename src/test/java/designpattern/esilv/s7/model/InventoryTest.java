48e4dd0cd5d3aac05706ba79fb90711999c1a5e2
Unknown <victor.seg2@gmail.com>
2018-12-14 10:06:52 +0100

    🌸 JUnit ArrayList (le retour)

---
+++ src/test/java/designpattern/esilv/s7/model/InventoryTest.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpattern.esilv.s7.model;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author victorseguin
+ */
+public class InventoryTest {
+    protected Inventory inv;
+    ArrayList<Item> ancienneListeDesItems;
+
+    @Before
+    public void setUp()
+    {
+        inv = new Inventory();
+        ancienneListeDesItems = inv.getItems();
+    }
+
+    @Test
+    public void testQualityIsNeverNegative() throws Exception
+    {
+        ArrayList<Item> items = inv.getItems();
+        
+        int nbIterations = 10;
+        for (int i=0;i < nbIterations;i++)
+        {
+            inv.updateQuality();
+            items = inv.getItems();
+            for(Item item : items){
+                assertTrue(item.getQuality() >= 0);
+            }
+        }
+    }
+
+    @Test
+    public void testQualityAgedBrie() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+        for (Item item : items)
+        {
+            if (item.getName().equals("Aged Brie"))
+            {
+                for(int i = 0; i < ancienneListeDesItems.size(); i++)
+                {
+                    if(ancienneListeDesItems.get(i).getName() == "Aged Brie")
+                    {
+                        assertTrue(item.getQuality() >= ancienneListeDesItems.get(i).getQuality());
+                    }
+                }
+            }
+        }
+    }
+    @Test
+    public void testSulfurasNotDecreaseInQuality() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+        
+        for (Item item : items)
+        {
+            if (item.getName().equals("Sulfuras, Hand of Ragnaros"))
+            {
+                for(int i = 0; i < ancienneListeDesItems.size(); i++){
+                    if(ancienneListeDesItems.get(i).getName() == "Sulfuras, Hand of Ragnaros"){
+                        assertTrue(item.getQuality() == ancienneListeDesItems.get(i).getQuality());
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testSulfurasNeverHasToBeSold() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+        
+        for (Item item : items)
+        {
+            if (item.getName().equals("Sulfuras, Hand of Ragnaros"))
+            {
+                for(int i = 0; i < ancienneListeDesItems.size(); i++){
+                    if(ancienneListeDesItems.get(i).getName() == "Sulfuras, Hand of Ragnaros"){
+                        assertFalse(item.getSellIn() < ancienneListeDesItems.get(i).getSellIn());
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testSulfurasQualityIs80() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+        
+        for (Item item : items)
+        {
+            if (item.getName().equals("Sulfuras, Hand of Ragnaros"))
+            {
+                assertTrue(item.getQuality() == 80);
+            }
+        }
+    }
+
+    @Test
+    public void testBackStagePassesQualityIncreasesBy2When10OrLess() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+        
+        for (Item item : items)
+        {
+            if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert"))
+            {
+                for(int i = 0; i < ancienneListeDesItems.size(); i++){
+                    if(ancienneListeDesItems.get(i).getName() == "Backstage passes to a TAFKAL80ETC concert" && item.getSellIn() <= 10){
+                         assertEquals(item.getQuality(),ancienneListeDesItems.get(i).getQuality() + 2);
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testBackStagePassesQualityIncreasesBy3When5OrLess() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+        for (Item item : items)
+        {
+            if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert"))
+            {
+                for(int i = 0; i < ancienneListeDesItems.size(); i++){
+                    if(ancienneListeDesItems.get(i).getName() == "Backstage passes to a TAFKAL80ETC concert" && item.getSellIn() <= 5){
+                         assertEquals(item.getQuality(),ancienneListeDesItems.get(i).getQuality() + 3);
+                    }
+                }
+            }
+        }
+    }
+    
+    @Test
+    public void testBackStagePassesQualityGoesToZero() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+
+        for (Item item : items)
+        {
+            if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert"))
+            {
+                for(int i = 0; i < ancienneListeDesItems.size(); i++){
+                    if(ancienneListeDesItems.get(i).getName() == "Backstage passes to a TAFKAL80ETC concert" && item.getSellIn() >= 0){
+                        assertTrue(item.getQuality() == 0);
+                    }
+                }
+            }
+        }
+    }
+    
+    @Test
+    public void QualityItemIsNotMoreThanFifty() throws Exception
+    {
+        inv.updateQuality();
+        ArrayList<Item> items = inv.getItems();
+
+        for (Item item : items)
+        {
+            if (! item.getName().equals("Sulfuras, Hand of Ragnaros"))
+            {
+                assertTrue(item.getQuality() <= 50);
+            }
+        }
+    }
+
+
+    
+    @Test
+    public void updateConjuredCakeQuality(){
+
+        ArrayList<Item> items = inv.getItems();
+        inv.updateQuality();
+        
+        for (Item item : items)
+        {
+            if (item.getName().equals("Conjured Mana Cake"))
+            {
+                for(int i = 0; i < ancienneListeDesItems.size(); i++)
+                {
+                    if(ancienneListeDesItems.get(i).getName() == "Conjured Mana Cake")
+                    { 
+                        assertEquals(item.getQuality(),ancienneListeDesItems.get(i).getQuality() + 2);
+                    }
+                }
+            }
+        }
+    }
+}
+
